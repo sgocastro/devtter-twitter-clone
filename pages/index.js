e3930bd0c1bac500812aaef1d6d491e54967d11e
santiagomcastro <sacastro@andreani.com>
2021-07-06 18:43:47 -0300

    Cambios para probar funcionalidad

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+        <Link href="/timeline">Timeline</Link>
       </main>
 
       <footer className={styles.footer}>
